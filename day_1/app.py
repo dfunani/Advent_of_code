2fd984f7c619a2369f0bbea81a98d2e8a020af94
DelaliFunani <dfunani@live.co.za>
2024-12-11 21:48:26 +0000

    day 1 answer 22565391

--- day_1/app.py
+++ day_1/app.py
@@ -1,16 +1,24 @@
-with open("data.txt") as file:
+with open("./data.txt") as file:
     sum = 0
     lefts = []
     rights = []
+    frequency = 0
     for line in file.readlines():
         left, right = line.split(" " * 3)
-        lefts.append(left)
-        rights.append(right)
+        lefts.append(int(left))
+        rights.append(int(right))
+
     lefts.sort()
     rights.sort()
-    print(lefts)
-    print(rights)
+    print(len(rights), len(lefts))
     for left, right in zip(lefts, rights):
         sum += abs(int(left) - int(right))
-    print(sum)
-    
+        count = rights.count(left)
+        
+        frequency += int(left) * count
+
+    with open("./result.txt", "w") as file:
+        file.write(f"Sum: {sum}\nFrequency Total: {frequency}")
+
+
+
